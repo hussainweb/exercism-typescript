c8a9e2716886ee8cdbbca93158ee683b8d831336
hw <hussainweb@gmail.com>
2023-03-14 12:14:39 -0400

    solve resistor-color-duo

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,16 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+const bandColors: { [key: string]: number } = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export function decodedValue(colors: string[]): number {
+  return bandColors[colors[0]] * 10 + bandColors[colors[1]];
 }
