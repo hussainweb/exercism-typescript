ef52e1ead06157059f8c6b351ff433c000acc4b6
hw <hussainweb@gmail.com>
2023-03-14 12:28:42 -0400

    solve resistor-color-trio

--- resistor-color-trio/resistor-color-trio.ts
+++ resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,29 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+const bandColors: { [key: string]: number } = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+const units = ["ohms", "kiloohms", "megaohms", "gigaohms"];
+
+export function decodedValue(colors: string[]): number {
+  let resistance = bandColors[colors[0]] * 10 + bandColors[colors[1]];
+  return resistance * Math.pow(10, bandColors[colors[2]]);
+}
+
+export function decodedResistorValue(colors: string[]): string {
+  let resistance = decodedValue(colors);
+  let unit = 0;
+  while (resistance > 1000) {
+    resistance = resistance / 1000;
+    unit++;
+  }
+  return `${resistance} ${units[unit]}`;
 }
